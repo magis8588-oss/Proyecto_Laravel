daafde86648931984afe25107edee8274b653380
DESKTOP-GDBHCCP\Danny <jose.arias38851@ucaldas.edu.co>
2025-12-01 12:50:44 -0500

    v2.3.1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Sistema de Citas Oftalmológicas') }}</title>
+        
+        <!-- Favicon - Emoji de ojo -->
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👁️</text></svg>">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
